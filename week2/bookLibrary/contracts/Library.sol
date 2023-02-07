decd54468d159b344971474a5cde5c539572946e
Plamen Damyanov <dupzzy37@gmail.com>
2023-02-08 01:50:21 +0200

    Adding full project, with 100% coverage + events to the contract

---
+++ week2/bookLibrary/contracts/Library.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+pragma abicoder v2;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+
+contract Library is Ownable {
+
+    struct Book {
+        bool firstAdd;
+        uint32 availableCopies;     
+        string title; 
+        address[] borrowingHistory;
+        mapping(address => bool) currentBorrowers; 
+    }
+
+    mapping(bytes32 => Book) public books; 
+    bytes32[] public iterator;           
+    
+    // Checking if such a book was ever added to the library
+    // There is a case where user is trying to borrow a book, and there are 0 left
+    // If there are 0 left, there are either borrowed or never added
+    // In case they are borrowed, user can expect them to be returned at some point
+    modifier checkTitle(string memory _title) {
+        bytes32 key = keccak256(abi.encodePacked(_title));
+        require(books[key].firstAdd == true, "Library doesn't contain the specified book");
+        _;
+    }
+
+    function addBooks(string memory _title) public onlyOwner {
+        bytes32 key = keccak256(abi.encodePacked(_title));
+
+        if(!books[key].firstAdd) {
+            books[key].title = _title;
+            books[key].availableCopies++;
+            books[key].firstAdd = true;      
+            iterator.push(key);
+        } else {
+            books[key].availableCopies++;
+        }
+
+        return;
+    }
+
+    function borrowBook(string memory _title) public checkTitle(_title) {
+        bytes32 key = keccak256(abi.encodePacked(_title));
+        require(books[key].availableCopies > 0, "There are no copies available from this book");
+        require(!books[key].currentBorrowers[msg.sender], "The user has already borrowed this book");
+
+        books[key].availableCopies--;
+        books[key].currentBorrowers[msg.sender] = true;
+        books[key].borrowingHistory.push(msg.sender);
+
+        return;
+    }
+
+    function returnBook(string memory _title) public checkTitle(_title) {
+        bytes32 key = keccak256(abi.encodePacked(_title));
+        require(books[key].currentBorrowers[msg.sender], "User has not borrowed such a book");
+
+        books[key].availableCopies++;
+        books[key].currentBorrowers[msg.sender] = false;
+    }
+    
+    function getNumberOfBooks() public view returns(uint) {
+        return iterator.length;
+    }
+
+    function getBorrowingHistory(string memory _title) public view checkTitle(_title) returns(address[] memory) {
+        bytes32 key = keccak256(abi.encodePacked(_title));
+        return books[key].borrowingHistory;
+    }
+}
---
+++ week2/bookLibrary/test/Library.ts
@@ -0,0 +1,73 @@
+import { Library } from "./../typechain-types/contracts/Library";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Library", function () {
+    let LibraryFactory;
+    let Library: Library;
+    let owner, addr1;
+
+    before(async () => {
+        [owner, addr1] = await ethers.getSigners();
+        LibraryFactory = await ethers.getContractFactory("Library");
+        Library = await LibraryFactory.deploy();
+        await Library.deployed();
+    });
+
+
+    it("Should fail to add book", async function () {
+        expect(Library.connect(addr1).addBooks("RandomTitle")).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+
+    for(let i = 1; i < 3; i++) 
+        it("Should add book (RandomTitle) " + i, async function () {
+            await Library.addBooks("RandomTitle");
+            expect(await Library.getNumberOfBooks()).to.equal(1);
+            expect(await Library.books(Library.iterator(0))).to.eql([true, i, "RandomTitle"]);
+        });
+
+    it("Should add new unique book", async function () {
+        await Library.addBooks("RandomTitle2");
+        expect(await Library.getNumberOfBooks()).to.equal(2);
+    });
+
+    it("Should borrow a book", async function () {
+        expect(Library.borrowBook("RandomTitle")).to.ok;
+    });
+
+    it("Should revert with book does not exist", async function () {
+        expect(Library.borrowBook("123")).to.be.revertedWith('Book does not exist');
+    });
+
+    // Functionality of currentBorrowers is covered in this test case
+    it("Should fail to borrow book", async function () {
+        expect(Library.borrowBook("RandomTitle")).to.be.revertedWith("The sender has already borrowed this book");
+    });
+
+    it("Should revert with no available copies", async function () {
+        await Library.connect(addr1).borrowBook("RandomTitle");
+        expect(Library.connect(addr1).borrowBook("RandomTitle")).to.be.revertedWith("No copies");
+    });
+
+    it("Should revert with this book was never borrowed", async function () {
+        expect(Library.returnBook("1234")).to.be.revertedWith('This book does not belong to the lib');
+    });
+
+    it("Should return borrowed book by owner", async function () {
+        await Library.returnBook("RandomTitle");
+    });
+
+    it("Should revert with user hasn't borrowed this book", async function () {
+        expect(Library.returnBook("RandomTitle")).to.be.revertedWith("User hasn't borrow this book");
+    });
+
+    it("Should revert with no such title", async function() {
+        expect(Library.getBorrowingHistory("RandomTitle123")).to.be.revertedWith("No such title");
+    });
+
+    // Checking if the addresses that were used when borrowing books are present in the borrowings history
+    it("Should get borrowing history", async function() {
+        const a = await Library.getBorrowingHistory("RandomTitle");
+        expect(a).to.eql([await owner.getAddress(), await addr1.getAddress()]);
+    });
+});
